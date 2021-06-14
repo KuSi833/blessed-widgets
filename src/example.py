dff9b266e86950147860c2aa0e820fa9bad4c928
Karlo <e14912km@student.manchester.ac.uk>
2021-06-14 22:21:31 +0200

    Padding overflow

--- src/example.py
+++ src/example.py
@@ -1,12 +1,17 @@
+from ssl import HAS_ALPN
+from xml.dom import VALIDATION_ERR
 from widgets import Square, Window, Button, Point
 from blessed import Terminal
+from constants import HAlignment, VAlignment
 
 term = Terminal()
 
 window = Window(term)
-enterButton = Button(window, Point(2, 2), "Check")
-square = Square(window, Point(2, 2), Point(6, 6), term.on_orange)
+enterButton = Button(window, Point(12, 12), Point(26, 14), "Check",
+                     text_color=term.black, bg_color=term.on_orange,
+                     h_align=HAlignment.LEFT, v_align=VAlignment.BOTTOM,
+                     padding=[2, 5, 2, 5])
 
 window.clear()
-square.draw()
-# enterButton.draw()
+enterButton.draw()
+print(term.home)
