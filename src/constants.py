927729b4ee1731b173ef1577aac867a4d2fbc70e
Karlo <e14912km@student.manchester.ac.uk>
2021-06-14 21:01:21 +0200

    alignment enums

---
+++ src/constants.py
@@ -0,0 +1,13 @@
+from enum import Enum, auto
+
+
+class HAlignment(Enum):
+    LEFT = auto
+    MIDDLE = auto
+    RIGHT = auto
+
+
+class VAlignment(Enum):
+    TOP = auto
+    MIDDLE = auto
+    BOTTOM = auto
